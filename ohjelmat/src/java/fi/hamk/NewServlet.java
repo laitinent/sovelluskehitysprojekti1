8c81f2d88456e2dac86da32bb57d90b2fb81d7bc
laitinent <toni.laitinen@yahoo.com>
2014-02-17 09:05:25 +0200

    init

---
+++ ohjelmat/src/java/fi/hamk/NewServlet.java
@@ -0,0 +1,125 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fi.hamk;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.Resource;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.sql.DataSource;
+
+/**
+ *
+ * @author tlaitinen
+ */
+public class NewServlet extends HttpServlet {
+    @Resource(name = "test")
+    private DataSource test;
+
+    /**
+     * Processes requests for both HTTP
+     * <code>GET</code> and
+     * <code>POST</code> methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet NewServlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet NewServlet at " + request.getContextPath() + "</h1>");
+            String u = request.getParameter("user");
+            String p = request.getParameter("programs");
+            String d = request.getParameter("descriptions");
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                Connection con = null;
+                con = DriverManager.getConnection("jdbc:mysql://localhost/test", "root", "");
+                Statement stmt = null;
+                stmt = con.createStatement();
+                
+                PreparedStatement statement = con.prepareStatement("INSERT INTO ohjelmat (user,programs,descriptions) VALUES (?,?,?)");
+                statement.setString(1, u);
+                statement.setString(2, p);
+                statement.setString(3, d);
+                statement.execute();
+                //stmt.executeUpdate("INSERT INTO ohjelmat (user,programs,descriptions) VALUES ('" + u + "','" + p + "','" + d + "')");
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+            catch (ClassNotFoundException ex) {
+                System.out.println(ex.getMessage());
+            }
+
+            out.println("</body>");
+            out.println("</html>");
+        } finally {
+            out.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP
+     * <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP
+     * <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
